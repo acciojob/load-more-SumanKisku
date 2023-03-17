0c025ce71776f0f81f104ec9255ac764a5ed3df2
Suman Kisku <sumankisku1@gmail.com>
2023-03-17 15:46:43 +0530

    test

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 
 import React from "react";
+import {useState} from "react"
 import './../styles/App.css';
 
 const items = [
@@ -34,13 +35,38 @@ const items = [
   "Item 29",
   "Item 30"
 ];
+const [list, setList] = useState([]);
+
+function addMore() {
+	setList(() => {
+
+	})
+}
 
 const App = () => {
+	const [list, setList] = useState([]);
+	const [firstIndex, setFirstIndex] = useState(0);
+	const [secondIndex, setSecondIndex] = useState(10);
+
+	function addMore() {
+		setList((prevList) => {
+			setFirstIndex(firstIndex+10);
+			setSecondIndex(secondIndex+10);
+			return prevList.concat(items.slice(firstIndex, secondIndex));
+		})
+	}
+
+
   return (
     <div>
         {/* Do not remove the main div */}
+	  <ul>
+		{list.map((item) => <li>{item}</li>)}
+	  </ul>
+	  <button onClick={addMore}>Load More</button>
     </div>
   )
+	
 }
 
 export default App
